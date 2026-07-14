b1e330996d9338da24362201a0a93f07612b9026
sayaliravan9316 <sayaliravan398@gmail.com>
2026-07-14 19:49:51 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import pickle
+#import pickle
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
